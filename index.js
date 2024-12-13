9cbf643ab760cb7a24879df12919eeb0674287f5
Bilal raza <attari1235@gmail.com>
2024-12-13 11:07:06 +0500

    authentication module updated

--- index.js
+++ index.js
@@ -1,48 +1,25 @@
 import express from "express";
 import morgan from "morgan";
-const tasks = [
-  {
-    id: 1,
-    task: "Sona nahn he",
-  },
-  {
-    id: 2,
-    task: "Jagna he",
-  },
-  {
-    id: 3,
-    task: "Concept samjna he",
-  },
-];
+import "dotenv/config";
+import mongoose from "mongoose";
+import taskRoutes from "./routers/tasks.js";
+import authRoutes from "./routers/auth.js";
+import userRoutes from "./routers/users.js";
 const app = express();
 const PORT = 4000;
 
 app.use(morgan("tiny"));
 app.use(express.json());
 
-function middleware(req, res, next) {
-  req.requestBy = "Bilal Raza";
-  next();
-}
+mongoose
+  .connect(process.env.MONGODBURI)
+  .then(() => console.log("mongodb connected"))
+  .catch((err) => console.log("err=>", err));
 
-// app.use(middleware);
+app.get("/", (req, res) => res.send("Server is running"));
 
-app.get("/", (req, res) => {
-  console.log("req.requestBy=>", req.requestBy);
-  res.status(200).send(tasks);
-});
-
-app.post("/", (req, res) => {
-  console.log("req.body=>", req.body);
-  res.send("Post Request is Called");
-});
-
-app.put("/", (req, res) => {
-  res.send("Put Request is Called");
-});
-
-app.delete("/", (req, res) => {
-  res.send("Delete Request is Called");
-});
+app.use("/task", taskRoutes);
+app.use("/auth", authRoutes);
+app.use("/user", userRoutes);
 
 app.listen(PORT, () => console.log("Server is running on PORT " + PORT));
---
+++ middleware/authentication.js
@@ -0,0 +1,24 @@
+import sendResponse from "../helpers/sendResponse.js";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+import User from "../models/User.js";
+
+export async function authenticateUser(req, res, next) {
+  try {
+    const bearerToken = req?.headers?.authorization;
+    const token = bearerToken?.split(" ")[1];
+    if (!token) return sendResponse(res, 403, null, true, "Token not provided");
+    const decoded = jwt.verify(token, process.env.AUTH_SECRET);
+
+    if (decoded) {
+      const user = await User.findById(decoded._id);
+      if (!user) return sendResponse(res, 403, null, true, "User not found");
+      req.user = decoded;
+      next();
+    } else {
+      sendResponse(res, 500, null, true, "Something went wrong");
+    }
+  } catch (err) {
+    sendResponse(res, 500, null, true, "Something went wrong");
+  }
+}
