eeb9566a96bcfd6d44d90e24cf09c6f56838cc5e
Bilal raza <attari1235@gmail.com>
2024-12-13 09:30:11 +0500

    Revert "connected mongodb"
    This reverts commit 2f02edb87f7109faeee52fab7643ab4af47ddc92.

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
 import express from "express";
-import userRoutes from "./routers/users.js";
-import mongoose from "mongoose";
-
+import morgan from "morgan";
+const tasks = [
+  {
+    id: 1,
+    task: "Sona nahn he",
+  },
+  {
+    id: 2,
+    task: "Jagna he",
+  },
+  {
+    id: 3,
+    task: "Concept samjna he",
+  },
+];
 const app = express();
 const PORT = 4000;
 
-mongoose
-  .connect("mongodb+srv://blog:blog@blog-app.ke01i.mongodb.net/test")
-  .then(() => console.log("Mongodb connected"))
-  .catch((err) => console.log("err=>", err));
+app.use(morgan("tiny"));
 app.use(express.json());
 
-app.use("/user", userRoutes);
+function middleware(req, res, next) {
+  req.requestBy = "Bilal Raza";
+  next();
+}
+
+// app.use(middleware);
+
+app.get("/", (req, res) => {
+  console.log("req.requestBy=>", req.requestBy);
+  res.status(200).send(tasks);
+});
+
+app.post("/", (req, res) => {
+  console.log("req.body=>", req.body);
+  res.send("Post Request is Called");
+});
+
+app.put("/", (req, res) => {
+  res.send("Put Request is Called");
+});
+
+app.delete("/", (req, res) => {
+  res.send("Delete Request is Called");
+});
 
 app.listen(PORT, () => console.log("Server is running on PORT " + PORT));
